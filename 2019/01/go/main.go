8b29d015db57aefe0d50271631880a2eb2824bc7
Mohamed Meftah <mohamed-ted@live.com>
2019-12-01 13:24:36 +0100

    day 01 part 01

--- 2019/01/go/main.go
+++ 2019/01/go/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
 
+func inputFromFile(path string) ([]int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var values []int
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		value, err := strconv.Atoi(scanner.Text())
+		if err == nil {
+			values = append(values, value)
+		}
+	}
+	return values, scanner.Err()
+}
+
+func main() {
+	input, err := inputFromFile("2019/01/go/input")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	sum := 0
+	for _, v := range input {
+		sum += int(math.Floor(float64(v)/3.0)) - 2
+	}
+
+	fmt.Println(sum)
 }
