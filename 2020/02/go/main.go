08f97c356f46b395d887a8de76e36e3825374d43
Mohamed Meftah <mohamed-ted@live.com>
2020-12-02 20:24:51 +0100

    solution for day 2

--- 2020/02/go/main.go
+++ 2020/02/go/main.go
@@ -2,22 +2,44 @@ package main
 
 import (
 	"bufio"
-	"io"
-	"strconv"
+	"fmt"
+	"os"
 )
 
-func inputToIntArray(r io.Reader) ([]int, error) {
-	var values []int
-	scanner := bufio.NewScanner(r)
+func isValidPasswordPartOne(input string) bool {
+	var min int
+	var max int
+	var c rune
+	var password string
 
-	for scanner.Scan() {
-		value, err := strconv.Atoi(scanner.Text())
-		if err == nil {
-			values = append(values, value)
+	fmt.Sscanf(input, "%d-%d %c: %s", &min, &max, &c, &password)
+
+	count := 0
+	for _, cc := range password {
+		if cc == c {
+			count++
 		}
 	}
-	return values, scanner.Err()
+	return count >= min && count <= max
 }
 
+func isValidPasswordPartTwo(input string) bool {
+	var pos1 int
+	var pos2 int
+	var c rune
+	var password string
+
+	fmt.Sscanf(input, "%d-%d %c: %s", &pos1, &pos2, &c, &password)
+
+	return password[pos1-1] != password[pos2-1] && (password[pos1-1] == byte(c) || password[pos2-1] == byte(c))
+}
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	count := 0
+	for scanner.Scan() {
+		if isValidPasswordPartTwo(scanner.Text()) {
+			count++
+		}
+	}
+	fmt.Println(count)
 }
