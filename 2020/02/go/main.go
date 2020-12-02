a6bb1dea9413e1ef3facd725eda6b48e72b37a39
Mohamed Meftah <mohamed-ted@live.com>
2020-12-02 20:26:09 +0100

    clean up

--- 2020/02/go/main.go
+++ 2020/02/go/main.go
@@ -6,22 +6,22 @@ import (
 	"os"
 )
 
-func isValidPasswordPartOne(input string) bool {
-	var min int
-	var max int
-	var c rune
-	var password string
+// func isValidPasswordPartOne(input string) bool {
+// 	var min int
+// 	var max int
+// 	var c rune
+// 	var password string
 
-	fmt.Sscanf(input, "%d-%d %c: %s", &min, &max, &c, &password)
+// 	fmt.Sscanf(input, "%d-%d %c: %s", &min, &max, &c, &password)
 
-	count := 0
-	for _, cc := range password {
-		if cc == c {
-			count++
-		}
-	}
-	return count >= min && count <= max
-}
+// 	count := 0
+// 	for _, cc := range password {
+// 		if cc == c {
+// 			count++
+// 		}
+// 	}
+// 	return count >= min && count <= max
+// }
 
 func isValidPasswordPartTwo(input string) bool {
 	var pos1 int
@@ -33,6 +33,7 @@ func isValidPasswordPartTwo(input string) bool {
 
 	return password[pos1-1] != password[pos2-1] && (password[pos1-1] == byte(c) || password[pos2-1] == byte(c))
 }
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	count := 0
